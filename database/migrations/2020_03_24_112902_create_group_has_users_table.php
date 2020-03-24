2d01f57f53749c67850b0a86480f4f6dbf605ad4
Soban3 <sobanfarooq3@gmail.com>
2020-03-24 16:31:17 +0500

    Model and migrations for group messaging.

--- database/migrations/2020_03_24_112902_create_group_has_users_table.php
+++ database/migrations/2020_03_24_112902_create_group_has_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateMessagesTable extends Migration
+class CreateGroupHasUsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,10 @@ class CreateMessagesTable extends Migration
      */
     public function up()
     {
-        Schema::create('messages', function (Blueprint $table) {
+        Schema::create('group_has_users', function (Blueprint $table) {
             $table->id();
+            $table->integer('user_id');
             $table->integer('group_id');
-            $table->integer('sender_id');
-            $table->text('message_text');
             $table->timestamps();
         });
     }
@@ -29,6 +28,6 @@ class CreateMessagesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('messages');
+        Schema::dropIfExists('group_has_users');
     }
 }
